202b020e2fe4c9a49f1b0b76dc204914ac2abc91
Nasir Ahmed Momin <nasir.momin@mutualmobile.com>
2022-06-18 17:18:49 +0530

    KMM Date creation

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -23,7 +23,11 @@ kotlin {
     }
     
     sourceSets {
-        val commonMain by getting
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.2")
+            }
+        }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
